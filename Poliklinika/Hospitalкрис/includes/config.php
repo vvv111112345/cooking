1ddd484133fb377590816dd453e40a0766ee3672
vvv111112345 <vyudin245@gmail.com>
2026-02-28 12:14:50 +0500

    Delete Poliklinika/Hospitalкрис directory

--- Poliklinika/Hospitalкрис/includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once 'db.php';
-
-// Настройки сайта
-define('SITE_NAME', 'Детская поликлиника №1');
-define('SITE_URL', 'http://localhost/detskaya_poliklinika');
-?>
